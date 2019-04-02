39f28256c06543f8fbd3685349cb780429c0718f
John Williams <palaiologos1990@gmail.com>
2019-04-02 11:48:42 -0700

    Differentiate between BC and AD dates in input

--- app/src/main/java/in/williams/john/randomhistory/MainActivity.java
+++ app/src/main/java/in/williams/john/randomhistory/MainActivity.java
@@ -19,19 +19,18 @@ import java.util.List;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    // Layout features.
     TextView responseText;
-
     EditText inputYear;
-
     Button submitButton;
-    Button eventButton;
-    Button birthButton;
 
-
+    // Strigns to hold years and URLs.
     String yearString;
     String baseURL;
+    String reformedYearString;
 
     boolean event;
+    boolean bc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,40 +41,18 @@ public class MainActivity extends AppCompatActivity {
         responseText =(TextView) findViewById(R.id.history_fact);
         submitButton =(Button) findViewById(R.id.button_submit);
 
-        // Toggleable buttons.
-        eventButton =(Button) findViewById(R.id.button_event);
-        birthButton =(Button) findViewById(R.id.button_birth);
-
         // Whatever the user writes.
         inputYear = (EditText) findViewById(R.id.edit_text_year);
 
         // Default setup for searching.
         event = true;
+        bc = false;
         // Default year is 2019.
         yearString = "2019";
+        reformedYearString = "";
+
         // Base URL for the wiki page.
         baseURL = "https://en.wikipedia.org/wiki/";
-
-
-
-
-        // Based on what user wants returned.
-        eventButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Execute the 'getRandomFact' class.
-                event = true;
-            }
-        });
-        // Finds famous births.
-        birthButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Execute the 'getRandomFact' class.
-                event = false;
-            }
-        });
-
 
         // Upon clicking the submit button.
         submitButton.setOnClickListener(new View.OnClickListener() {
@@ -83,6 +60,12 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 // Get the user's input as a string.
                 yearString = inputYear.getText().toString();
+
+                // Find if BC or AD year.
+                bc = checkBCorAD(yearString);
+
+                // Remove all non-digit parts of the string with regex.
+                reformedYearString = yearString.replaceAll("\\D+", "");
 
                 // Execute the 'getRandomFact' class.
                 new getRandomFact().execute();
@@ -99,66 +82,68 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected Void doInBackground(Void... voids) {
 
-            // Get an event, not a birth.
-            if (event) {
-                try {
-                    // Make array of strings to hold our facts.
-                    List<String> factsList = new ArrayList<>();
-                    // URL.
-                    String fullURL = baseURL + yearString;
-                    // Get the entire web page.
-                    Document doc = Jsoup.connect(fullURL).get();
-                    // Get the unordered lists under the div.
-                    Element div = doc.select("div.mw-parser-output").first();
-                    // Get all the children of the div.
-                    Element element = div.children().first();
-                    // Counter to check how many <h2> tags we've seen.
-                    int flag = 0;
+            try {
+                // Make array of strings to hold our facts.
+                List<String> factsList = new ArrayList<>();
 
-                    // While we have only seen one <h2> tag, keep looping.
-                    while (flag <= 1) {
-                        // If an <h2> appears, increment flag counter.
-                        if (element.tagName() == "h2" ) {
-                            flag++;
-                            // Break out if we hit another section.
-                            if (flag > 1) {
-                                break;
-                            }
+                String fullURL = "";
+
+                // URL determination.
+                if (bc) {
+                    fullURL = baseURL + reformedYearString + "_BC";
+                } else {
+                    fullURL = baseURL + "AD_" + reformedYearString;
+                }
+
+                // Get the entire web page.
+                Document doc = Jsoup.connect(fullURL).get();
+                // Get the unordered lists under the div.
+                Element div = doc.select("div.mw-parser-output").first();
+                // Get all the children of the div.
+                Element element = div.children().first();
+                // Counter to check how many <h2> tags we've seen.
+                int flag = 0;
+
+                // While we have only seen one <h2> tag, keep looping.
+                while (flag <= 1) {
+                    // If an <h2> appears, increment flag counter.
+                    if (element.tagName() == "h2" ) {
+                        flag++;
+                        // Break out if we hit another section.
+                        if (flag > 1) {
+                            break;
                         }
-                        // Else, it's normal and we continue to find <ul> elements.
-                        else {
-                            // If we find an <ul>, get all its <li> elements.
-                            if (element.tagName().equals("ul") ) {
-                                // Get all <li> items.
-                                Elements li = element.children();
-                                int size = li.size();
-                                // Put each <li> text into our factsList array list.
-                                for (int i = 0; i < size; i++) {
-                                    factsList.add(li.get(i).text());
+                    }
+                    // Else, it's normal and we continue to find <ul> elements.
+                    else {
+                        // If we find an <ul>, get all its <li> elements.
+                        if (element.tagName().equals("ul") ) {
+                            // Get all <li> items.
+                            Elements li = element.children();
+                            int size = li.size();
+                            // Put each <li> text into our factsList array list.
+                            for (int i = 0; i < size; i++) {
+                                String fact = li.get(i).text();
+                                // Do not add empty facts.
+                                if (fact.length() > 0) {
+                                    factsList.add(fact);
                                 }
                             }
                         }
-                        // Progress to next sibling.
-                        element = element.nextElementSibling();
-                    } // End looping through all div children.
+                    }
+                    // Progress to next sibling.
+                    element = element.nextElementSibling();
+                } // End looping through all div children.
 
-                    // Print out a random fact based on random function.
-                    words = factsList.get(makeRandomNum(factsList.size()));
+                // Print out a random fact based on random function.
+                words = factsList.get(makeRandomNum(factsList.size()));
 
-                } catch(Exception e) {e.printStackTrace();}
-
-            }
-
-            // Otherwise, get a birth.
-            else {
-                try {
-                    Document doc = Jsoup.connect(baseURL + yearString).get();
-
-                    words = doc.text();
-
-                } catch(Exception e) {e.printStackTrace();}
-
-            }
+                /*
+                for (int i = 0; i < factsList.size(); i++) {
+                    words += factsList.get(i);
+                }
+                */
+            } catch(Exception e) {e.printStackTrace();}
 
 
             return null;
@@ -182,8 +167,14 @@ public class MainActivity extends AppCompatActivity {
         return randomNum;
     }
 
+    // Check if the input string has "BC" or "bc" in it.
+    public boolean checkBCorAD(String s) {
+        if (s.indexOf("bc") != -1 || s.indexOf("BC") != -1 ) {
+            return true;
+        }
 
-
+        return false;
+    }
 
 
 
